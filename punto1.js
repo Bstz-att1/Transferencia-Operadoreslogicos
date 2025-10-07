9525997204b9a462ea881d1e43c569ca528cbbdf
Bstz-att1 <hfdarioalexander7@gmail.com>
2025-10-06 20:50:39 -0500

    Final punto 1 - Añadido comentarios y pregunta analisis

--- punto1.js
+++ punto1.js
@@ -1,8 +1,12 @@
-let usuario = prompt("Ingrese su nombre de usuario");
-let contraseña = prompt("Ingrese su contraseña");
+let usuario = prompt("Ingrese su nombre de usuario"); // solicita el nombre de usuario
+let contraseña = prompt("Ingrese su contraseña"); // solicita la contraseña del usuario
 
-if (usuario === "admin" && contraseña === "1234") {
-    alert("Acceso concedido");
+// verifica si el usuario y la contraseña son correctos
+
+if (usuario === "admin" && contraseña === "1234") { // ambas condiciones deben ser verdaderas para conceder acceso
+    alert("Acceso concedido"); // muestra un mensaje de acceso concedido si las credenciales son correctas
 } else {
-    alert("Acceso denegado");
+    alert("Acceso denegado"); // muestra un mensaje de acceso denegado si alguna de las credenciales es incorrecta
 }
+
+//el operador logico que use fue el AND (&&) porque ambas condiciones deben ser verdaderas para que se conceda el acceso.
